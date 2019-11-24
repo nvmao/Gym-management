76614a33869b75e2b7ab9bc9b17c1ffcf12da995
Mao Nguyen <newman33399@gmail.com>
2019-11-24 18:43:36 +0700

    done login form

---
+++ src/com/mao/Main.java
@@ -0,0 +1,45 @@
+package com.mao;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    private Stage stage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        this.stage = primaryStage;
+        mainWindow();
+    }
+
+
+    private void mainWindow(){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
+
+            AnchorPane root = loader.load();
+            Scene scene =  new Scene(root);
+
+            scene.getStylesheets().addAll(getClass().getResource("style.css").toExternalForm());
+
+            stage.setTitle("Sign in");
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (IOException e){
+            System.out.println(e);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
